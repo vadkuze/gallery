3e91f63243ba929303b79ea4fd8dc5070f108c4d
Vadim Kuznetsov <vadkuze@gmail.com>
2017-10-10 14:47:59 +0300

    app photo gallery done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { AlertModule } from 'ngx-bootstrap';
+import {  Routes, RouterModule  } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageDetailComponent } from './image/image-detail.component';
+import { ImageService } from './shared/images.service';
+import { ImageFilterPipe } from './shared/filter.pipe';
+import { appRoutes } from './route';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GalleryComponent,
+    ImageDetailComponent,
+    ImageFilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    AlertModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ImageService, ImageFilterPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/gallery/gallery.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { ImageService } from '../shared/images.service';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.css']
+})
+export class GalleryComponent implements OnChanges {
+  title = 'Recent Photos';
+  @Input() filterBy?: string = 'all'
+  visibleImages: any[] = [];
+
+  constructor(private imageService: ImageService) {
+    this.visibleImages = this.imageService.getImages();
+   }
+
+  ngOnChanges() {
+    this.visibleImages = this.imageService.getImages();
+    
+  }
+
+}
